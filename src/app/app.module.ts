8dcaf7a17eb1f7ab7b0a6ad0831cffebeeb32d11
_lmmmmmm <931420654@qq.com>
2021-12-23 11:59:18 +0800

    [特性](deps): 删除 typeorm, mysql2 依赖

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Connection } from 'typeorm';
 
-import { TypeOrmConfig } from '../configs/typeorm.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../modules/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(TypeOrmConfig), UserModule],
   controllers: [AppController],
   providers: [AppService],
+  imports: [UserModule],
 })
-export class AppModule {
-  constructor(private connection: Connection) {}
-}
+export class AppModule {}
